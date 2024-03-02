671d2bc7fb05c30c87e732d384054ff20ed0a102
kadiebwe oscar <kadiebweoscar0@gmail.com>
2024-03-02 23:28:23 +0100

    add a images of the design(maket) and add a texte for tilte

--- src/components/recipePage.jsx
+++ src/components/recipePage.jsx
@@ -1,11 +1,12 @@
 import imgOmelette from "./images/image-omelette.jpeg"
 export default function RecipePage() {
   return (
-    <>
-    recipePage
-    <figure>
-        <img src={imgOmelette} alt="image omelette" />
-    </figure>
-    </>
+    <section>
+      <figure>
+          <img src={imgOmelette} alt="image omelette" />
+      </figure>
+      <h1>Simple Omelette Recipe</h1>
+      <p>An easy and quick dish, perfect for ony meal. This classic omelette combines beaten eggs cooked to perfection, optionally filled with your choice of cheese, vegetables, or meats.</p>
+    </section>
   )
 }
