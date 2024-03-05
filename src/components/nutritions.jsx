2678e82613ed1e9c915d59fc7d56449b9598af0f
kadiebwe oscar <kadiebweoscar0@gmail.com>
2024-03-05 23:56:00 +0100

    add: a flexbox in style

--- src/components/nutritions.jsx
+++ src/components/nutritions.jsx
@@ -4,22 +4,22 @@ export default function Nutritions() {
     <div>
       <h3>Nutritions</h3>
       <p>The table below shows nutritional valuess per serving without the additional fillings.</p>
-      <span>
+      <span className="flex">
         <p>Calories</p>
         <b>277kcal</b>
       </span>
 
-      <span>
+      <span className="flex">
         <p>Carbs</p>
         <b>0g</b>
       </span>
 
-      <span>
+      <span className="flex">
         <p>Protein</p>
         <b>20g</b>
       </span>
 
-      <span>
+      <span className="flex">
         <p>Fat</p>
         <b>22g</b>
       </span>
